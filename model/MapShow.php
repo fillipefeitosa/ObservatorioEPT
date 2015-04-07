73ef0118c4e796a2916b5cca293f7e8c418a73b0
Fillipe Oliveira Feitosa <fillfeitosa@gmail.com>
2015-04-06 22:57:11 -0300

    Show Map

--- model/MapShow.php
+++ model/MapShow.php
@@ -1,4 +1,27 @@
 <?php
-	
+
+	class MapShow{
+
+		$link = null;
+		$desc = null;
+
+		function __construct($desc){
+  	if(!isset($desc)){
+  		throw new Exception("</br></br>Mapa não definido.</br></br>");
+		}else{
+			$this->desc = $desc;
+		}
+		}
+
+		function showMap(){
+			switch($this->link){
+				case "relacao_ifs":
+					return "<iframe width='500' height='300' scrolling='no' frameborder='no' src='https://www.google.com/fusiontables/embedviz?q=select+col3+from+17iLYeOl9s2FXtTbNAJNJ3kJX18Z-kFfE_ua_5gPj&amp;viz=MAP&amp;h=false&amp;lat=-7.479365221253366&amp;lng=-45.933205825000016&amp;t=1&amp;z=4&amp;l=col3&amp;y=2&amp;tmplt=2&amp;hml=ONE_COL_LAT_LNG'></iframe>";
+
+			}
+
+		}
+
+	}
 
 ?>
--- view/view_map.php
+++ view/view_map.php
@@ -1,27 +1,27 @@
 <?php
 	include_once('head.php');
+	include_once('/model/MapShow.php');
 
 	$mapDesc = $_GET['desc'];
-	
-?>	
-	
-	
+
+	$objMap = new MapShow($mapDesc);
+?>
+
+
 	<div class="large-9 push-3 columns">
-          
+
           <h3>Observatório da Educação Profissional e Tecnológica </br>
           <small>Uma ferramenta de acompanhamento da EPT no Brasil</small></h3>
-          
+
           <p>Aqui haverá uma descrição sucinta do projeto.</p>
           <ul>Coisas que devem aparecer no menu ao lado.
           	<li>Sobre</li>
           	<li>Mapas -> submenus</li>
-          	<li>Acesso à base de dados</li>          	
-                
+          	<li>Acesso à base de dados</li>
+
  </div>
-	
-	
-<?php	
+
+
+<?php
 	include_once('footer.php');
 ?>
-	
-	
