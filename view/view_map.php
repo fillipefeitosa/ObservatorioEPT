00f4851e324731f625952448e2213b4820284e26
Fillipe Feitosa <fillfeitosa@gmail.com>
2015-05-17 14:57:39 -0300

    Ajustando Tamanho do Mapa

--- view/view_map.php
+++ view/view_map.php
@@ -21,11 +21,11 @@
 				<?php
 					switch($mapDesc){
 						case "relacao_ifs":
-							echo "<iframe width='500' height='300' scrolling='no' frameborder='no' src='https://www.google.com/fusiontables/embedviz?q=select+col3+from+17iLYeOl9s2FXtTbNAJNJ3kJX18Z-kFfE_ua_5gPj&amp;viz=MAP&amp;h=false&amp;lat=-7.479365221253366&amp;lng=-45.933205825000016&amp;t=1&amp;z=4&amp;l=col3&amp;y=2&amp;tmplt=2&amp;hml=ONE_COL_LAT_LNG'></iframe>";
+							echo "<iframe width='800' height='400' scrolling='no' frameborder='no' src='https://www.google.com/fusiontables/embedviz?q=select+col3+from+17iLYeOl9s2FXtTbNAJNJ3kJX18Z-kFfE_ua_5gPj&amp;viz=MAP&amp;h=false&amp;lat=-7.479365221253366&amp;lng=-45.933205825000016&amp;t=1&amp;z=4&amp;l=col3&amp;y=2&amp;tmplt=2&amp;hml=ONE_COL_LAT_LNG'></iframe>";
 						break;
 
 						case "heatmap":
-							echo "<iframe width='500' height='300' scrolling='no' frameborder='no' src='https://www.google.com/fusiontables/embedviz?q=select+col3+from+17iLYeOl9s2FXtTbNAJNJ3kJX18Z-kFfE_ua_5gPj+limit+1000&amp;viz=HEATMAP&amp;h=true&amp;lat=-14.559456373761329&amp;lng=-36.20182004999998&amp;t=1&amp;z=4&amp;l=col3&amp;y=3&amp;tmplt=3&amp;hmd=true&amp;hmg=%2366ff0000%2C%2393ff00ff%2C%23c1ff00ff%2C%23eeff00ff%2C%23f4e300ff%2C%23f4e300ff%2C%23f9c600ff%2C%23ffaa00ff%2C%23ff7100ff%2C%23ff3900ff%2C%23ff0000ff&amp;hmo=0.6&amp;hmr=25&amp;hmw=0&amp;hml=ONE_COL_LAT_LNG'></iframe>";
+							echo "<iframe width='800' height='400' scrolling='no' frameborder='no' src='https://www.google.com/fusiontables/embedviz?q=select+col3+from+17iLYeOl9s2FXtTbNAJNJ3kJX18Z-kFfE_ua_5gPj+limit+1000&amp;viz=HEATMAP&amp;h=true&amp;lat=-14.559456373761329&amp;lng=-36.20182004999998&amp;t=1&amp;z=4&amp;l=col3&amp;y=3&amp;tmplt=3&amp;hmd=true&amp;hmg=%2366ff0000%2C%2393ff00ff%2C%23c1ff00ff%2C%23eeff00ff%2C%23f4e300ff%2C%23f4e300ff%2C%23f9c600ff%2C%23ffaa00ff%2C%23ff7100ff%2C%23ff3900ff%2C%23ff0000ff&amp;hmo=0.6&amp;hmr=25&amp;hmw=0&amp;hml=ONE_COL_LAT_LNG'></iframe>";
 						break;
 
 						case "pibXifs":
